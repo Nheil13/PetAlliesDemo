f69f3b81aef43fb3473f5d84474a06c757407a80
Nheil13 <95905981+Nheil13@users.noreply.github.com>
2023-10-28 17:15:00 +0800

    Update Dashboard.java

--- app/src/main/java/com/example/petallies/Dashboard.java
+++ app/src/main/java/com/example/petallies/Dashboard.java
@@ -17,14 +17,46 @@ public class Dashboard extends AppCompatActivity {
         setContentView(R.layout.dashboard);
 
         ImageView announcementBanner = findViewById(R.id.announcementBanner);
+        ImageView imageProduct = findViewById(R.id.imageProduct);
+        ImageView imageServices = findViewById(R.id.imageServices);
+        ImageView imageProfile = findViewById(R.id.imageProfile);
+        ImageView imageAboutUs = findViewById(R.id.imageAboutUs);
 
         String urlAnnouncementBanner = (String) announcementBanner.getTag();
+        String urlProduct = (String) imageProduct.getTag();
+        String urlServices = (String) imageServices.getTag();
+        String urlProfile = (String) imageProfile.getTag();
+        String urlAboutUs = (String) imageAboutUs.getTag();
 
         Picasso.get()
                 .load(urlAnnouncementBanner)
                 .memoryPolicy(MemoryPolicy.NO_CACHE, MemoryPolicy.NO_STORE) // Pumipigil sa memory caching
                 .networkPolicy(NetworkPolicy.NO_CACHE) // Pumipigil sa network caching
                 .into(announcementBanner);
+
+        Picasso.get()
+                .load(urlProduct)
+                .memoryPolicy(MemoryPolicy.NO_CACHE, MemoryPolicy.NO_STORE) // Pumipigil sa memory caching
+                .networkPolicy(NetworkPolicy.NO_CACHE) // Pumipigil sa network caching
+                .into(imageProduct);
+
+        Picasso.get()
+                .load(urlServices)
+                .memoryPolicy(MemoryPolicy.NO_CACHE, MemoryPolicy.NO_STORE) // Pumipigil sa memory caching
+                .networkPolicy(NetworkPolicy.NO_CACHE) // Pumipigil sa network caching
+                .into(imageServices);
+
+        Picasso.get()
+                .load(urlProfile)
+                .memoryPolicy(MemoryPolicy.NO_CACHE, MemoryPolicy.NO_STORE) // Pumipigil sa memory caching
+                .networkPolicy(NetworkPolicy.NO_CACHE) // Pumipigil sa network caching
+                .into(imageProfile);
+
+        Picasso.get()
+                .load(urlAboutUs)
+                .memoryPolicy(MemoryPolicy.NO_CACHE, MemoryPolicy.NO_STORE) // Pumipigil sa memory caching
+                .networkPolicy(NetworkPolicy.NO_CACHE) // Pumipigil sa network caching
+                .into(imageAboutUs);
 
 
 
